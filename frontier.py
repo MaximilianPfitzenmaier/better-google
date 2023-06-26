8ab77f1b0b8867469ba127b18e049bee6d42bb1c
FrogInABlender <pati.lenhart@web.de>
2023-06-26 16:19:43 +0200

    added frontier list - wip

---
+++ frontier.py
@@ -0,0 +1,19 @@
+#Urls angeordnet in der READ_ME Reihenfolge
+
+frontier = [
+    'https://hoelderlinturm.de/english/', 'https://www.tuebingen.de/en/'
+    'https://www.my-stuwe.de/en/', 'https://guide.michelin.com/en/de/baden-wurttemberg/tbingen/restaurants', # reichen solche guides?
+    'https://uni-tuebingen.de/en/facilities/central-institutions/university-sports-center/home/', # reichen solche datenbanken?
+    'https://uni-tuebingen.de/en/', 'https://civis.eu/en/about-civis/universities/eberhard-karls-universitat-tubingen', 'https://tuebingenresearchcampus.com/', 'https://is.mpg.de/'
+    'https://www.tripadvisor.com/Attractions-g198539-Activities-Tubingen_Baden_Wurttemberg.html', # noch mehr guides, decken aber gut ab - zumal eh die einzelnen seiten idr nur auf deutsch sind
+    'https://www.medizin.uni-tuebingen.de/en-de/startseite', 'https://apps.allianzworldwidecare.com/poi/hospital-doctor-and-health-practitioner-finder?PROVTYPE=PRACTITIONERS&TRANS=Doctors%20and%20Health%20Practitioners%20in%20Tuebingen,%20Germany&CON=Europe&COUNTRY=Germany&CITY=Tuebingen&choice=en', 'https://www.yelp.com/search?cflt=physicians&find_loc=T%C3%BCbingen%2C+Baden-W%C3%BCrttemberg%2C+Germany',
+    'https://cyber-valley.de/', 'https://www.tuebingen.mpg.de/84547/cyber-valley',
+    'https://tuebingen.ai/', 'https://www.eml-unitue.de/',
+    'https://en.wikipedia.org/wiki/T%C3%BCbingen', 'https://wikitravel.org/en/T%C3%BCbingen',
+    'https://www.bahnhof.de/en/tuebingen-hbf',
+    # politics
+    # geograpy
+    'https://www.engelvoelkers.com/en-de/properties/rent-apartment/baden-wurttemberg/tubingen-kreis/',
+    'https://integreat.app/tuebingen/en/news/tu-news', 'https://tunewsinternational.com/category/news-in-english/' # news
+]
+
