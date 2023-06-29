de5eb0e4e9d85355c7551654d21cd8e6f94046fa
Maximilian Pfitzenmaier <max.pfitzenmaier@googlemail.com>
2023-06-29 20:15:06 +0200

    added missing props

--- web_crawler.py
+++ web_crawler.py
@@ -9,6 +9,7 @@ from urllib.robotparser import RobotFileParser
 from bs4 import BeautifulSoup
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
 from rake_nltk import Rake
 from database import Database
 
@@ -16,7 +17,7 @@ from database import Database
 class Crawler:
     initial_frontier = [
         'https://hoelderlinturm.de/english/',
-        'https://www.tuebingen.de/en/'
+        'https://www.tuebingen.de/en/',
         # reichen solche guides?
         'https://www.my-stuwe.de/en/',
         'https://guide.michelin.com/en/de/baden-wurttemberg/tbingen/restaurants',
@@ -54,6 +55,7 @@ class Crawler:
         nltk.download('punkt')
         nltk.download('wordnet')
         nltk.download('stopwords')
+
         # If the frontier is empty, we load it with our initial frontier
         if self.db.check_frontier_empty():
             for link in self.initial_frontier:
@@ -73,13 +75,17 @@ class Crawler:
         print(f"ID: {web_page['id']}")
         print(f"URL: {web_page['url']}")
         print(f"Title: {web_page['title']}")
+        print(f"Normalized Title: {web_page['normalized_title']}")
         print(f"Keywords: {web_page['keywords']}")
         print(f"Description: {web_page['description']}")
+        print(f"Normalized Description: {web_page['normalized_description']}")
         # print(f"Internal Links: {web_page['internal_links']}")
         # print(f"External Links: {web_page['external_links']}")
         # print(f"In Links: {web_page['in_links']}")
         # print(f"Out Links: {web_page['out_links']}")
         # print(f"Content: {web_page['content']}")
+        print(f"Image URL: {web_page['img']}")
+
         print("--------------------")
 
     def add_internal_links_to_frontier(self, url, internal_links):
@@ -136,26 +142,37 @@ class Crawler:
                 # only crawl the page content, if the content is english
                 if is_page_language_english(soup, url):
                     # Extract the title, keywords, description, internal/external links, content
+                    index = None
                     title = get_page_title(soup)
+                    normalized_title = get_normalized_title(
+                        title) if title else None
                     description = get_description(soup)
-                    internal_links = get_internal_external_links(soup)[0]
-                    external_links = get_internal_external_links(soup)[1]
+                    normalized_description = get_normalized_description(
+                        description) if description else None
+                    internal_links = get_internal_external_links(soup, url)[0]
+                    external_links = get_internal_external_links(soup, url)[1]
                     content = get_page_content(soup)
-                    keywords = get_keywords(soup, content)
+                    keywords = get_keywords(
+                        content, normalized_title, normalized_description)
                     in_links = []
                     out_links = []
+                    img = get_image_url(soup, url)
 
                     # Create the web page object
                     web_page = create_web_page_object(
                         url,
+                        index,
                         title,
+                        normalized_title,
                         keywords,
                         description,
+                        normalized_description,
                         internal_links,
                         external_links,
                         in_links,
                         out_links,
                         content,
+                        img,
                     )
 
                     # Save to the database
@@ -262,29 +279,37 @@ def is_page_language_english(soup, url):
 # Crawler Functions
 def create_web_page_object(
     url,
+    index,
     title,
+    normalized_title,
     keywords,
     description,
+    normalized_description,
     internal_links,
     external_links,
     in_links,
     out_links,
     content,
+    img,
 ):
     """
     Creates a web page object.
 
     Parameters:
     - id (int): The ID of the web page.
+    - index (int | none): The index of the web page.
     - url (str): The URL of the web page.
     - title (str): The title of the web page.
+    - normalized_title (str): The normalized title of the web page.
     - keywords (list): The keywords generated from the content.
     - description (str): The description from the description meta tag.
+    - normalized description (str): The normalized description from the description meta tag.
     - internal_links (list): A list of internal links (URLs within the same domain).
     - external_links (list): A list of external links (URLs outside the current domain).
     - in_links (list): An empty list to store incoming links from other pages.
     - out_links (list): A list of URLs extracted from anchor tags on the page.
     - content (str): The plain text content of the web page.
+    - img (str): The URL to the thumbnail.
 
     Returns:
     - dictionary: A dictionary representing the web page object.
@@ -293,14 +318,18 @@ def create_web_page_object(
     return {
         # 'id': page_id,
         'url': url,
+        'index': index,
         'title': title,
+        'normalized_title': normalized_title,
         'keywords': keywords,
         'description': description,
+        'normalized_description': normalized_description,
         'internal_links': internal_links,
         'external_links': external_links,
         'in_links': in_links,
         'out_links': out_links,
         'content': content,
+        'img': img,
     }
 
 
@@ -351,19 +380,65 @@ def get_page_title(soup):
     return soup.title.string if soup.title else None
 
 
-def get_keywords(soup, content):
+def get_normalized_title(title):
+    """
+    Normalize and lemmatize the given title.
+
+    Parameters:
+    - title (str): The input title to be normalized.
+
+    Returns:
+    - str: The normalized and lemmatized version of the title.
+    """
+
+    # Remove special characters and lowercase the content
+    content = re.sub(r'[^\w\s]', '', title).lower()
+
+    # Normalize German characters to English equivalents
+    content = normalize_german_chars(content)
+
+    # Tokenize the content
+    tokens = word_tokenize(content)
+
+    # Remove stopwords
+    stopwords_set = set(stopwords.words('english'))
+    filtered_tokens = [token for token in tokens if token not in stopwords_set]
+
+    # Lemmatize the content
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_content = [lemmatizer.lemmatize(
+        token) for token in filtered_tokens]
+
+    # Convert the lemmatized content back to a string
+    lemmatized_content_str = ' '.join(lemmatized_content)
+
+    return lemmatized_content_str
+
+
+def get_keywords(content, normalized_title, normalized_description):
     """
     Extracts the keywords from the content using the RAKE algorithm.
 
     Parameters:
     - content (str): The text content to extract keywords from.
+    - normalized_title (str): The normalized_title to extract keywords from.
+    - normalized_description (str): The normalized_description to extract keywords from.
 
     Returns:
     - list: The extracted keywords as a list of single words.
     """
+
+    concat = ""
+    if content is not None:
+        concat += content + " "
+    if normalized_title is not None:
+        concat += normalized_title
+    if normalized_description is not None:
+        concat += normalized_description
+
     # Create an instance of the Rake object
     r = Rake()
-    r.extract_keywords_from_text(content)
+    r.extract_keywords_from_text(concat)
     ranked_phrases_with_scores = r.get_ranked_phrases_with_scores()
 
     # Filter out phrases with more than one word
@@ -376,8 +451,8 @@ def get_keywords(soup, content):
     # Deduplicate keywords (case-insensitive)
     unique_keywords = list(set(map(str.lower, single_words)))
 
-    # Limit to the top 20 single words
-    keywords = unique_keywords[:20]
+    # Limit to the top single words
+    keywords = unique_keywords
 
     return keywords
 
@@ -396,27 +471,65 @@ def get_description(soup):
     return description['content'] if description else None
 
 
-def get_internal_external_links(soup):
+def get_normalized_description(description):
+    """
+    Normalize and lemmatize the given description.
+
+    Parameters:
+    - description (str): The input description to be normalized.
+
+    Returns:
+    - str: The normalized and lemmatized version of the description.
+    """
+    # Remove special characters and lowercase the content
+    content = re.sub(r'[^\w\s]', '', description).lower()
+
+    # Normalize German characters to English equivalents
+    content = normalize_german_chars(content)
+
+    # Tokenize the content
+    tokens = word_tokenize(content)
+
+    # Remove stopwords
+    stopwords_set = set(stopwords.words('english'))
+    filtered_tokens = [token for token in tokens if token not in stopwords_set]
+
+    # Lemmatize the content
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_content = [lemmatizer.lemmatize(
+        token) for token in filtered_tokens]
+
+    # Convert the lemmatized content back to a string
+    lemmatized_content_str = ' '.join(lemmatized_content)
+
+    return lemmatized_content_str
+
+
+def get_internal_external_links(soup, url):
     """
     Extracts the internal and external links from a web page from the given BeautifulSoup object.
 
     Parameters:
     - soup (BeautifulSoup): The BeautifulSoup object representing the parsed HTML content.
+    - url (str): The URL of the web page.
 
     Returns:
     - tuple: A tuple containing two lists:
         - list: The internal links (URLs within the same domain).
         - list: The external links (URLs outside the current domain).
     """
+    # get the base url
+    base_url = urljoin(url, '/')[:-1]
+
     internal_links = []
     external_links = []
     for link in soup.find_all('a'):
         href = link.get('href')
         if href:
             if href.startswith('http') or href.startswith('https'):
-                external_links.append(href)
+                external_links.append(base_url + href)
             else:
-                internal_links.append(href)
+                internal_links.append(base_url + href)
     return (internal_links, external_links)
 
 
@@ -452,6 +565,20 @@ def get_page_content(soup):
     # Normalize German characters to English equivalents
     content = normalize_german_chars(content)
 
+    #! enable later when we write our own keyword function
+    # # Tokenize the content
+    # tokens = word_tokenize(content)
+
+    # # Remove stopwords
+    # stopwords_set = set(stopwords.words('english'))
+    # filtered_tokens = [token for token in tokens if token not in stopwords_set]
+
+    # # Lemmatize the content
+    # lemmatizer = WordNetLemmatizer()
+    # lemmatized_content = [lemmatizer.lemmatize(
+    #     token) for token in filtered_tokens]
+
+    #! delete this later
     # Tokenize the content
     tokens = word_tokenize(content)
 
@@ -464,5 +591,48 @@ def get_page_content(soup):
 
     # Convert the lemmatized content back to a string
     lemmatized_content_str = ' '.join(lemmatized_content)
-    # return test
+
     return lemmatized_content_str
+
+
+def get_image_url(soup, url):
+    """
+    Extracts the og:twitter image URL or favicon URL from the given BeautifulSoup object.
+
+    Parameters:
+    - soup (BeautifulSoup): The BeautifulSoup object representing the parsed HTML content.
+    - url (str): The URL of the web page.
+
+    Returns:
+    - str: The og:image URL if found, otherwise the og:twitter image URL if found, otherwise the favicon URL, or an empty string if both are not found.
+    """
+    # Find the og:twitter image tag
+    og_image_tag = soup.find(
+        "meta", attrs={"property": "og:image"})
+
+    # Find the og:twitter image tag
+    twitter_image_tag = soup.find(
+        "meta", attrs={"property": "twitter:imag"})
+
+    # Find the favicon tag
+    favicon_tag = soup.find("link", rel="icon")
+
+    # Extract the og:image image URL
+    if og_image_tag is not None:
+        og_image_url = og_image_tag.get("content", "")
+        if og_image_url:
+            return og_image_url if og_image_url.startswith('http') else 'https:' + og_image_url
+
+    # Extract the twitter:image URL
+    elif twitter_image_tag is not None:
+        og_image_url = twitter_image_tag.get("content", "")
+        if og_image_url:
+            return og_image_url if og_image_url.startswith('http') else urljoin(url, '/')[:-1] + og_image_url
+
+    # Extract the favicon URL
+    elif favicon_tag is not None:
+        favicon_url = favicon_tag.get("href", "")
+        if favicon_url:
+            return urljoin(url, '/')[:-1] + favicon_url
+
+    return ""
